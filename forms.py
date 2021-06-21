11ec3089b0721274514bebcf548a6a48dd57f65e
Vankit-Verma <86182844+Vankit-Verma@users.noreply.github.com>
2021-06-21 19:37:56 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,12 @@
+
+from django import forms
+
+from website.FunLearning.models import user
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = user
+        widgets = {
+        'password': forms.PasswordInput(),
+    }
